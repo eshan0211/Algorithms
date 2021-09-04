e8a48724d4de6ccf48e61d030ee18b9e2df3693d
Eshan Madnani <55271617+eshan0211@users.noreply.github.com>
2021-09-05 00:07:39 +0530

    Update 12.py

--- Searching and sorting/Middle of Three/12.py
+++ Searching and sorting/Middle of Three/12.py
@@ -1,27 +1,20 @@
-String state;
-void setup()
-{
-  pinMode(12, OUTPUT);
-  Serial.begin(9600);
-}
-void loop()
-{
-  state=Serial.readString();
-  if(state=="ON"){
-    digitalWrite(12,HIGH);
-    Serial.println("ON");
-  delay(5000);
-}
-else if(state=="OFF"){
-  digitalWrite(12, LOW);
-    Serial.println("OFF");
-  delay(5000);
-}
-  else{
-    Serial.println("blink");
-     digitalWrite(12, HIGH);
-    delay(1000);
-    digitalWrite(12, LOW);
-     delay(1000);
-  }
-}
+class Solution:
+    def middle(self,a,b,c):
+        #code here
+ # Compare each three number to find
+    # middle number. Enter only if a > b
+        if a > b :
+            if (b > c):
+                return b
+            elif (a > c) :
+                return c
+            else :
+                return a
+        else:
+        # Decided a is not greater than b.
+            if (a > c) :
+                return a
+            elif (b > c) :
+                return c
+            else :
+                return b
